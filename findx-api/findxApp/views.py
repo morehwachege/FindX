a754bed36ffb71aac147c8ff80daa5e8eb4893f9
morehwachege <antony123muriithi@gmail.com>
2022-04-01 02:56:36 +0300

    serializer & api routes

--- findx-api/findxApp/views.py
+++ findx-api/findxApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.permissions import isAuthenticated
+from django.contrib.auth import get_user_model
+from . import UserSerializer
 # Create your views here.
+
+class UserSerializer(viewsets.ModelViewSet):
+    permission_classes = (isAuthenticated)
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
